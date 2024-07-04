cd4d6dd5dd6acb676d9b22b62d60a2558803a030
JoseLuisArrona <joseluisarrona@yahoo.com>
2024-07-03 21:17:25 -0600

    Se Terminan ejercicios javaScript y se crea la carpeta sobre modificar el Doom 03/07/2024

---
+++ ejercicios/Aritmetica basica.js
@@ -0,0 +1,20 @@
+let a = 5,
+    b = 2,
+    resultado = 0;
+    
+    resultado = a+b;
+    
+    resultado = a-b;
+    
+    resultado = a *b ;
+    
+    resultado = a /b ; 
+    
+    // MODULOS 
+    
+    resultado = a %b;
+    
+    //potencia 
+    
+    resultado = a **b;
+    console.log(resultado);
