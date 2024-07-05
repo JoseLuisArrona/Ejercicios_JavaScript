b397f9305eec699a8c55e8fa595d57d881a33646
JoseLuisArrona <joseluisarrona@yahoo.com>
2024-07-04 22:18:26 -0600

    Se Terminan ejercicios javaScript 04/07/2024

---
+++ ejercicios/Strings.js
@@ -0,0 +1,16 @@
+let apellido,
+    nombre,
+    nombreCompleto;
+
+    nombre = "Juan";
+    apellido ='Perez'
+
+nombreCompleto = nombre + apellido;
+
+console.log(nombreCompleto);
+
+console.log(`El apellido ingresado es: ${apellido} y su nombre es: ${nombre}`);
+
+console.log("El apellido registrado es: " + apellido);
+
+console.log('el significado de "DOM" es document object model.')
