7945818ae74617aaf6f1fd9e3df16e0099611b8e
JoseLuisArrona <joseluisarrona@yahoo.com>
2024-07-10 21:55:05 -0600

    Se Terminan ejercicios javaScript 10/07/2024

---
+++ ejercicios/funciones1.js
@@ -0,0 +1,10 @@
+function llamarjaimito(dinero,bolsa) //Cuando recibes Valores son Parametros 
+{
+    console.log('la compra fue exitosa');
+    let compra = "leche";
+    return compra;
+}
+
+let recibiAlgo = llamarjaimito("dinero","bolsa") // cuando mandas valores son Argumentos 
+
+console.log(recibiAlgo);
