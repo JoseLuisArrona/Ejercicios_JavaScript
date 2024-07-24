7ab37bb4f63339779fae2a5a8a3538b9b53d4b0f
JoseLuisArrona <joseluisarrona@yahoo.com>
2024-07-23 22:21:55 -0600

    Se Terminan ejercicios javaScript 23/07/2024

---
+++ ejercicios/objetoMath.js
@@ -0,0 +1,37 @@
+// object MATH
+// obtener el PI
+console.log(Math.PI);
+
+//Mostrar solo 2 decimales
+console.log(Math.PI.toFixed(2));
+
+//quita los decimales 
+console.log(Math.floor(Math.PI));
+
+//redondea un numero
+console.log(Math.round(4.8));
+console.log(Math.round(Math.PI));
+
+//pasar un numero negativo a su valor absoluto o positivo
+console.log(Math.abs(-404));
+
+//calcular la potencia 
+console.log(Math.pow(2,5));
+console.log(2**3);
+
+
+//------
+
+// numero aleatoria entre 0 y 1  
+console.log(Math.random());
+
+//numero aleatoria entre 1 y 10 
+console.log(Math.random() *10 +1);
+
+//numero random entre 1 y 100
+console.log(Math.floor(Math.random()*100+1))
+
+
+
+
+
---
+++ ejercicios/parseInt-parseFloat.js
@@ -0,0 +1,15 @@
+let cadena1 = '28'
+let cadena2= '28.33'
+let cadena3 = '28 dias tiene febrero'
+let cadena4 = '28.3'
+
+//parseInt se usa para convertir un numero en ENTERO
+let resultado1 = parseInt(cadena1);
+console.log(resultado1);
+console.log(typeof resultado1);
+
+//parseFloat se usa pa convertir un numro de punto flotante o con decimales
+
+let resultado2 = parseFloat(cadena2);
+console.log(resultado2)
+console.log(typeof resultado2);
